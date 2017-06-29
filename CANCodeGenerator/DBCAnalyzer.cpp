6bcf1d7fa05273657906b6496348fc03567d909a
Even <liyiwen007@gmail.com>
2017-06-29 18:38:28 +0800

    增加DBC文件的Message和Signal解析的代码，可以将DBC文件中的Message内容和Signal解析成DBCFileDescriptor中的Message和Signal成员。
    Add parsing code to generate Message and Signal Descriptor and add it into the DBCFileDescriptor.

--- CANCodeGenerator/DBCAnalyzer.cpp
+++ CANCodeGenerator/DBCAnalyzer.cpp
@@ -1,5 +1,6 @@
 #include "DBCAnalyzer.h"
 
+#include <regex>
 
 
 DBCAnalyzer::DBCAnalyzer()
@@ -14,6 +15,65 @@ DBCAnalyzer::~DBCAnalyzer()
 DBCFileDescriptor DBCAnalyzer::Analyze(std::string const & _file_name)
 {
 	DBCFileDescriptor file_descriptor;
+		
+	//AnalyzerDBCByLines();
 
 	return std::move(file_descriptor);
+}
+
+void DBCAnalyzer::AnalyzerDBCByLines(std::vector<std::string> const & _lines)
+{
+	DBCFileDescriptor file_descriptor;
+	for (auto iter = _lines.begin(); iter != _lines.end(); ++iter) {
+		if (MessageRecognizer(*iter, file_descriptor))
+		{
+			auto & msg = file_descriptor.Messages().back();
+			++iter;
+			while (SignalRecognizer(*iter, msg)) {
+				++iter;
+			}
+			--iter;
+		}
+	}
+}
+
+bool DBCAnalyzer::MessageRecognizer(std::string const & _line, DBCFileDescriptor & _file_descriptor)
+{
+	// message = BO_ message_id message_name ':' message_size transmitter{ signal };
+	std::regex message_definition(R"(BO_\s+(\d+)\s+(\w+)\s+:.*)");
+	std::smatch m;
+	if (!std::regex_match(_line, m, message_definition)) {
+		return false;
+	}
+	
+	Message msg;
+	msg.SetMessageName(m[2].str());
+	msg.SetMsgID(std::stol(m[1].str()));
+
+	_file_descriptor.AddMessage(msg);
+
+	return true;
+}
+
+bool DBCAnalyzer::SignalRecognizer(std::string const & _line, Message & _msg)
+{
+	//signal = 'SG_' signal_name multiplexer_indicator ':' start_bit '|'
+	//	signal_size '@' byte_order value_type '(' factor ',' offset ')'
+	//	'[' minimum '|' maximum ']' unit receiver{ ',' receiver };
+	std::regex signal_definition(R"(\s*SG_\s+(\w+)\s+:\s+(\d+)\|(\d+)@(\d)(\+|-).*)");
+	std::smatch m;
+	if (!std::regex_match(_line, m, signal_definition)) {
+		return false;
+	}
+	Signal signal;
+	signal.SetName(m[1].str());
+	signal.SetBitInfo(
+		static_cast<uint8_t>(std::stoul(m[2].str())), 
+		static_cast<uint8_t>(std::stoul(m[3].str())));
+	signal.SetIsBigEndian(m[4].str() == "1");
+	signal.SetIsUnsigned(m[5].str() == "+");
+
+	_msg.AddSignal(signal);
+	
+	return true;
 }
--- CANCodeGenerator/DBCAnalyzer.h
+++ CANCodeGenerator/DBCAnalyzer.h
@@ -6,23 +6,71 @@
 
 
 class Signal {
+public:
+	void SetName(std::string const _name) {
+		m_signal_name = _name;
+	}
+
+	void SetBitInfo(uint8_t _start, uint8_t _size) {
+		m_start_bit = _start;
+		m_signal_size = _size;
+	}
+
+	void SetIsBigEndian(bool _is_big_endian) {
+		m_is_big_endian = _is_big_endian;
+	}
+	void SetIsUnsigned(bool _is_unsigned) {
+		m_is_unsigned = _is_unsigned;
+	}
+
+private:
 	std::string m_signal_name;
 	uint8_t m_start_bit;
 	uint8_t m_signal_size;
+	bool    m_is_big_endian;
+	bool    m_is_unsigned;
 };
 
 class Message {
-	std::string m_message_name;
+
+public:
+	void SetMessageName(std::string const & _name) {
+		m_message_name = _name;
+	}
+	void SetMsgID(uint32_t _msg_id) { m_msg_id = _msg_id; }
+
+	void AddSignal(Signal const & _signal) {
+		m_signals.push_back(_signal);
+	}
+
+	std::vector<Signal> & Signals(void) { 
+		return m_signals; 
+	}
+private:
+	std::string         m_message_name;
+	uint32_t            m_msg_id;
 	std::vector<Signal> m_signals;
 };
 
 class DBCFileDescriptor {
+public:
+	void AddMessage(Message const & _msg) {
+		m_messages.push_back(_msg);
+	}
+	std::vector<Message> & Messages(void) {
+		return m_messages;
+	}
+
+private:
+
 	std::vector<Message> m_messages;
 };
 
 
 // @brief input a dbc file, output a DBCFileDescriptor for DBC
-//     DBC Ref to : <DBC-File-Format-Documentation.pdf>
+//        DBC Ref to : <DBC-File-Format-Documentation.pdf>
+//        Analyzer file by Regular Expression.
+//
 class DBCAnalyzer
 {
 public:
@@ -32,5 +80,12 @@ public:
 
 public:
 	DBCFileDescriptor Analyze(std::string const & _file_name);
+
+
+private:
+	void AnalyzerDBCByLines(std::vector<std::string> const & _lines);
+
+	bool MessageRecognizer(std::string const & _line, DBCFileDescriptor & _file_descriptor);
+	bool SignalRecognizer(std::string const & _line, Message & _msg);
 };
 
